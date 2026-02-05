9682e68c1a6828605836ec7f4921d2c41b173c56
unknown <jasminedhillon2003@gmail.com>
2026-02-05 20:53:59 +0530

    update

--- src/data/events.js
+++ src/data/events.js
@@ -1,33 +1,31 @@
 const events = [
   {
     id: 1,
-    title: "Arijit Singh Live Concert",
-    category: "Music",
-    date: "25 Feb 2026",
-    venue: "JLN Stadium, Delhi",
-    price: 999,
-    image:
-      "https://images.unsplash.com/photo-1507874457470-272b3c8d8ee2",
+    title: "Rambo Circus Live Concert",
+    category: "Kids",
+    date: "25 July 2026",
+    venue: "Amritsar Indoor Stadium",
+    price: 400,
+    image:"/images/rambo.jpg",
   },
   {
     id: 2,
-    title: "Stand-up Comedy Night",
+    title: "Open Mic Night",
     category: "Comedy",
     date: "2 Mar 2026",
-    venue: "Phoenix Mall, Mumbai",
+    venue: "Nexus Mall,Amritsar",
     price: 499,
-    image:
-      "https://images.unsplash.com/photo-1527224857830-43a7acc85238",
+    image: "/images/openmic.png",
   },
   {
     id: 3,
-    title: "Theatre Play – Hamlet",
-    category: "Theatre",
-    date: "10 Mar 2026",
-    venue: "Prithvi Theatre, Mumbai",
+    title: "Chalta Hai Comedy Show",
+    category: "Comedy",
+    date: "10 Aug 2026",
+    venue: "Multiple Venues",
     price: 799,
     image:
-      "https://images.unsplash.com/photo-1515169067865-5387ec356754",
+      "/images/comedy.png",
   },
 ];
 
